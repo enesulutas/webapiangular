5393b26855f84623830cda97e5a7e00f9392e9c6
HuseyinCengiz <hseyin.cengiz@hotmail.com>
2018-12-14 01:37:15 +0300

    Yapimci islemleri yapıldı.

--- src/app/material-widgets/input/input.component.ts
+++ src/app/material-widgets/input/input.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 import {
   FormControl,
   FormGroupDirective,
   NgForm,
   Validators
-} from "@angular/forms";
-import { Yapimci } from "../../classes/Yapimci";
-import { Oyun } from "../../classes/Oyun";
-import { YapimciService } from "../../shared/yapimci.service";
+} from '@angular/forms';
+import {Yapimci} from '../../classes/Yapimci';
+import {Oyun} from '../../classes/Oyun';
+import {YapimciService} from '../../shared/yapimci.service';
 
 @Component({
-  selector: "cdk-input",
-  templateUrl: "./input.component.html",
-  styleUrls: ["./input.component.scss"]
+  selector: 'cdk-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
-  public yapimci: Yapimci = new Yapimci(null, "", new Date(), "");
+  public yapimci: Yapimci = new Yapimci(null, '', new Date(), '');
   public oyun: Oyun = new Oyun();
 
-  public kategorilerim: string = "";
-  public kategoriler: string[] = ["enes", "huseyin", "muhammed"];
+  public kategorilerim: string = '';
+  public kategoriler: string[] = ['enes', 'huseyin', 'muhammed'];
 
-  public yapimcilarim: string = "";
+  public yapimcilarim: string = '';
   public yapimcilar: Yapimci[] = [];
 
-  constructor(private yapimciService: YapimciService) {}
+  constructor(private yapimciService: YapimciService) {
+  }
 
   ngOnInit() {
-	  this.getAllYapimci();
+    this.getAllYapimci();
   }
 
   onYapimciSubmit() {
@@ -41,9 +42,9 @@ export class InputComponent implements OnInit {
   getAllYapimci() {
     this.yapimciService.getAll().subscribe(
       data => {
-        this.yapimcilar=data as Yapimci[];
+        this.yapimcilar = data as Yapimci[];
       },
-      err => console.log("Yapımcıları çekme başarısız."),
+      err => console.log('Yapımcıları çekme başarısız.'),
       null
     );
   }
