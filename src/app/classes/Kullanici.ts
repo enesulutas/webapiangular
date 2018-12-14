7f6f4ee9293eacfb834bfd55e7a700c39bb4f04b
HuseyinCengiz <hseyin.cengiz@hotmail.com>
2018-12-14 17:41:57 +0300

    Login and Output feature were added.

---
+++ src/app/classes/Kullanici.ts
@@ -0,0 +1,8 @@
+export class Kullanici {
+    public id:number;
+    public adi:string;
+    public kullaniciAdi:string;
+    public dogumTarihi:Date;
+    public sifre:string;
+    public bakiye:number;
+}
