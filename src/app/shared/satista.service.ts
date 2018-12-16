69ec4d3e9ccfd4277bee7cef299bbdb508a60a30
Enes Ulutaş <enestein07@gmail.com>
2018-12-16 17:36:53 +0300

    satis

---
+++ src/app/shared/satista.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Satista } from '../classes/Satista';
+import { Observable } from "rxjs";
+import { Yapimci } from '../classes/Yapimci';
+import { Utility } from '../utils/utility';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SatistaService {
+
+constructor(private Http:HttpClient) { 
+}
+
+getAll():Observable<Satista[]>{
+  return this.Http.get<Satista[]>(Utility.Host+"satista");
+}
+
+}
